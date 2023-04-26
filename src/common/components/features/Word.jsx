be36858531e1f5c0e9ac2c951470f6dc1094591d
The Untitled <nguyenminhcuong2000@gmail.com>
2023-04-26 19:49:08 +0700

    #2023/04/26 chau - Done typing matches the word - [Beam translate] tomorrow

--- src/common/components/features/Word.jsx
+++ src/common/components/features/Word.jsx
@@ -1,12 +1,18 @@
 import { useStateContext } from "../../../contexts/ContextProvider";
+import Letter from "./Letter";
 
 const Word = ({ word, wordActiveIndex }) => {
-  const { activeIndex, onActiveWord, setOnActiveWord } = useStateContext();
+  const { activeIndex } = useStateContext();
 
   return (
     <div className={`word${wordActiveIndex === activeIndex ? " active" : ""}`}>
       {word.split("").map((char, index) => (
-        <span key={index}>{char}</span>
+        <Letter
+          key={index}
+          char={char}
+          isActive={wordActiveIndex === activeIndex}
+          letterIndex={index}
+        />
       ))}
     </div>
   );
